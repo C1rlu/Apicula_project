9bb2e0683dfa859dc19c6e122147a96a9cab727c
Cirlu <leprovostulric@gmail.com>
2023-12-10 20:43:45 +0100

    Revert "Update photo Npc"
    This reverts commit 72e2dd386ed867a183d2ca07467cd65ce83a6eaa.

--- 09_NPC/Npc_data.gd
+++ 09_NPC/Npc_data.gd
@@ -2,7 +2,6 @@ extends Resource
 class_name Npc_datas
 
 @export var name : String
-@export var image : Texture2D
+@export var image : Image
 @export var prefabs : PackedScene
 @export var Dialogue_data : JSON
-@export var photo_done : bool
