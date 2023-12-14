e05b8c863afcdd57786c3f9b8864e2a2729b3833
Cirlu <leprovostulric@gmail.com>
2023-12-14 14:01:02 +0100

    Fps locker

--- 00_PLAYER/Sonar/Sonar_behaviour.gd
+++ 00_PLAYER/Sonar/Sonar_behaviour.gd
@@ -1,31 +1,35 @@
 extends Node3D
 
 @export var _sonar_speed = 2.0
+@onready var timer = $Timer
+@onready var _1 = $"01"
+@onready var _2 = $"02"
 
 func _ready():
 	_global_datas._active_sonar.connect(active_sonar)
 	self.scale = Vector3(0.1,0.1,0.1)
-	$"01".visible = false
-	$"02".visible = false
+	_1.visible = false
+	_2.visible = false
 	
 func active_sonar():
-	if !$Timer.is_stopped():
+	if !timer.is_stopped():
 		return 
-	$Timer.start()
-	$"01".visible = true
-	$"02".visible = true
+	timer.start()
+	_1.visible = true
+	_2.visible = true
 	
 func _process(_delta):
-	if $Timer.is_stopped():
+
+	if 	timer.is_stopped():
 		return
-	var timer = ($Timer.wait_time - $Timer.time_left) * _delta *_sonar_speed  
-	if timer < 0.1:
+	var new_timer = (timer.wait_time - timer.time_left) *_sonar_speed 
+	if new_timer < 0.1:
 		return
-	self.scale = Vector3(timer,timer,timer)
+	self.scale = Vector3(new_timer,new_timer,new_timer)
 	
 
 func _on_timer_timeout():
 	self.scale = Vector3(0.1,0.1,0.1)
-	$"01".visible = false
-	$"02".visible = false
+	_1.visible = false
+	_2.visible = false
 	#_global_datas._disable_sonar.emit()
