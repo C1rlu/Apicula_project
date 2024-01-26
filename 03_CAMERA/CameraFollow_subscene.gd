6c57ae3be35ad3a7f2a652b93b3e261a7609cdb5
Cirlu <leprovostulric@gmail.com>
2024-01-26 15:32:03 +0100

    update

--- 03_CAMERA/CameraFollow_subscene.gd
+++ 03_CAMERA/CameraFollow_subscene.gd
@@ -2,48 +2,41 @@ extends Node
 
 var smooth_speed = 3.0
 var offset: Vector3
-var sub_scene_offset: Vector3
-var dialogue_offset: Vector3
 var active_target : Vector3
 
 @export var _all_cam_array : Array[Camera3D]
 
+var canMove = false
+
+@export var basePos: Vector3
+
 func _ready():
-	
-	sub_scene_offset = Vector3(0.0,-3.125,0.0)
-	dialogue_offset =  Vector3(3.125,0.0,0.0)
-	
-	var playerPosition = _global_datas.player_position
-	offset = _all_cam_array[0].transform.origin - playerPosition
+	var playerPosition = _global_datas.subbscene_playerPosition
+	offset = self.transform.origin - playerPosition
 	active_target = offset
 	
+	# global move
+	_global_datas._start_ini_subscene.connect(cant_move)
+	_global_datas._end_ini_subscene.connect(can_move)
 	
-	_global_datas._go_Subscene.connect(_inSubScene_states)
-	_global_datas._go_Mainscene.connect(_inMainScene_states)
+	basePos = active_target 
 	
-	_global_datas._open_dialogue.connect(_inDialogue)
-	_global_datas.in_dialogue_zone.connect(_outDialogue)
-	
-
-
 	
 func _physics_process(_delta):
 	
-	var playerPosition = _global_datas.player_position
+	if !canMove:
+		return
+	var playerPosition = _global_datas.subbscene_playerPosition	
 	for cam in _all_cam_array:
 		cam.transform.origin = lerp(cam.transform.origin, playerPosition + active_target, smooth_speed * _delta)	
 
 	
-	
-func _inSubScene_states():
-	active_target = offset + sub_scene_offset
-	
-func _inMainScene_states():
-	active_target = offset
 
-
-func _inDialogue():
-	active_target = offset + dialogue_offset
+func can_move():
+	canMove = true
 	
-func _outDialogue():
-	active_target = offset
+	
+func cant_move():
+	canMove = false
+	self.position = active_target 
+
