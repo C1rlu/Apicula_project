78fa9072f082faecfaf2e1708455e4a2638bf5e0
Cirlu <leprovostulric@gmail.com>
2024-02-05 18:04:12 +0100

    passion

--- 08_SUBSCENES/Elements/Object/Photo_element_zone/Element_photo_zone.gd
+++ 08_SUBSCENES/Elements/Object/Photo_element_zone/Element_photo_zone.gd
@@ -2,5 +2,3 @@ extends Area3D
 
 @export var photoData : PhotoData
 
-func _disable():
-	$Flash_vfx._disable()
--- 08_SUBSCENES/Player_subscene/Flash_inputs.gd
+++ 08_SUBSCENES/Player_subscene/Flash_inputs.gd
@@ -1,6 +1,7 @@
 extends Node
 
-@onready var on_over_mouse_3d_shape = $"../Mouse_Over_And_ZoneCollide/On_OverMouse_3D_SHAPE"
+@onready var on_over_mouse_3d_shape = $"../Mouse_Over_And_ZoneCollide/On_OverMouse_3D_SHAPE_out"
+@onready var on_over_mouse_3d_shape_in = $"../Mouse_Over_And_ZoneCollide/On_OverMouse_3D_SHAPE_in"
 
 var _can_flash : bool = false
 @onready var area = $"../Mouse_Over_And_ZoneCollide"
@@ -9,8 +10,7 @@ func _input(event):
 	
 	if !_can_flash:
 		return
-		
-		
+	
 	if _global_datas.Player_lock_click:
 		return
 	if event.is_action_pressed("Click"):
@@ -28,19 +28,19 @@ func check_area():
 		
 	for areas in overlap_areas_:
 		
-		var photo_data_node = areas.get_node_or_null("Photo_data")
-		var loot_data_node = areas.get_node_or_null("Lootable")
+		#var photo_data_node = areas.get_node_or_null("Photo_data")
+		#var loot_data_node = areas.get_node_or_null("Lootable")
 		var mirror_node = areas.get_node_or_null("Mirror")
 		
-		if  photo_data_node:
-			var photo_data_rs : PhotoData = photo_data_node.get_PhotoData()
-			_global_datas._instance_photo.emit(photo_data_rs)
-			return
+		#if  photo_data_node:
+			#var photo_data_rs : PhotoData = photo_data_node.get_PhotoData()
+			#_global_datas._instance_photo.emit(photo_data_rs)
+			#return
 			
-		if  loot_data_node:
-			var lootable_data_rs : Loot_Data = loot_data_node.get_lootable()
-			_global_datas._find_loot.emit(lootable_data_rs)		
-			return
+		#if  loot_data_node:
+			#var lootable_data_rs : Loot_Data = loot_data_node.get_lootable()
+			#_global_datas._find_loot.emit(lootable_data_rs)		
+			##return
 				
 		if mirror_node:
 			_global_datas._click_mirror_switch.emit()
@@ -48,13 +48,16 @@ func check_area():
 	
 	
 func _on_mouse_over_mouse_entered():
-	_can_flash = true
-	on_over_mouse_3d_shape.visible = _can_flash
-	
+	pass
+	#_can_flash = true
+	#on_over_mouse_3d_shape.visible = true
+
 
 func _on_mouse_over_mouse_exited():
-	_can_flash = false
-	on_over_mouse_3d_shape.visible = _can_flash
+	pass
+	#_can_flash = false
+	#on_over_mouse_3d_shape.visible = false
+
 
 
 
