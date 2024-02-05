da453811413efc902f47705769579c4c63bbf728
Cirlu <leprovostulric@gmail.com>
2024-02-05 21:20:35 +0100

    ups

--- 08_SUBSCENES/Elements/Mirror_zone/Flash_vfx.gd
+++ 08_SUBSCENES/Elements/Mirror_zone/Flash_vfx.gd
@@ -2,16 +2,12 @@ extends Node
 
 @onready var mirror_particule : GPUParticles3D = $"../Mirror_Particule"
 
-@onready var take_photo = $"../Take_Photo"
-
 func _on_element_photo_prefabs_3_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	if area.get_node_or_null("Player_photoActive"):
-		
+
 		mirror_particule.emitting = true
-		take_photo.in_zone = true
-		print("CAN CLICK")
+		print(area)
 func _on_element_photo_prefabs_3_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
-	
-	if take_photo.in_zone:
+	if area.get_node_or_null("Player_photoActive"):
 		mirror_particule.emitting = false
-		take_photo.in_zone = false
+	
--- 08_SUBSCENES/Player_subscene/Flash_inputs.gd
+++ 08_SUBSCENES/Player_subscene/Flash_inputs.gd
@@ -1,10 +1,11 @@
 extends Node
 
-@onready var on_over_mouse_3d_shape = $"../Mouse_Over_And_ZoneCollide/On_OverMouse_3D_SHAPE_out"
-@onready var on_over_mouse_3d_shape_in = $"../Mouse_Over_And_ZoneCollide/On_OverMouse_3D_SHAPE_in"
-
 var _can_flash : bool = false
-@onready var area = $"../Mouse_Over_And_ZoneCollide"
+@onready var area = $"../ZoneCollide"
+var count : int = 0
+
+@onready var on_over_mouse_3d_shape_out = $"../On_Click/On_OverMouse_3D_SHAPE_out"
+
 
 func _input(event):
 	
@@ -28,35 +29,36 @@ func check_area():
 		
 	for areas in overlap_areas_:
 		
-		#var photo_data_node = areas.get_node_or_null("Photo_data")
-		#var loot_data_node = areas.get_node_or_null("Lootable")
+		var photo_data_node = areas.get_node_or_null("Photo_data")
+		var loot_data_node = areas.get_node_or_null("Lootable")
 		var mirror_node = areas.get_node_or_null("Mirror")
 		
-		#if  photo_data_node:
-			#var photo_data_rs : PhotoData = photo_data_node.get_PhotoData()
-			#_global_datas._instance_photo.emit(photo_data_rs)
-			#return
+		if  photo_data_node:
+			var photo_data_rs : PhotoData = photo_data_node.get_PhotoData()
+			_global_datas._instance_photo.emit(photo_data_rs)
+
+			return
 			
-		#if  loot_data_node:
-			#var lootable_data_rs : Loot_Data = loot_data_node.get_lootable()
-			#_global_datas._find_loot.emit(lootable_data_rs)		
-			##return
+		if  loot_data_node:
+			var lootable_data_rs : Loot_Data = loot_data_node.get_lootable()
+			_global_datas._find_loot.emit(lootable_data_rs)	
+
+			return
 				
 		if mirror_node:
 			_global_datas._click_mirror_switch.emit()
-
+	
 	
 	
 func _on_mouse_over_mouse_entered():
-	pass
-	#_can_flash = true
-	#on_over_mouse_3d_shape.visible = true
+	_can_flash = true
+	on_over_mouse_3d_shape_out.visible = true
 
 
 func _on_mouse_over_mouse_exited():
-	pass
-	#_can_flash = false
-	#on_over_mouse_3d_shape.visible = false
+
+	_can_flash = false
+	on_over_mouse_3d_shape_out.visible = false
 
 
 
