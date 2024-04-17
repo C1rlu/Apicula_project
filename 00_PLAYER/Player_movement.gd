a4251bb89256f40189a95246e7a35a508f205795
Cirlu <leprovostulric@gmail.com>
2024-04-17 22:05:18 +0200

    UPDATE

--- 00_PLAYER/Player_movement.gd
+++ 00_PLAYER/Player_movement.gd
@@ -4,29 +4,43 @@ extends RigidBody3D
 @export var maxSpeed = 10.0
 @export var rotation_speed = 0.5
 
+var using_pad : bool
+
+
+
+func _ready():
+	
+	_global_datas.using_pad.connect(_using_pad)
+	
+	
+func _using_pad(condition : bool):
+	
+	using_pad = condition	
+	
+	
 func _physics_process(_delta):
 	
-	move_a()
-
-func move_a():
-	
-	
-	
+	if _global_datas.Player_In_Inventory:
+		return	
+	if _global_datas.Player_InSubScene:
+		return
+	if _global_datas.Player_InMenu:
+		return	
+	if _global_datas.Player_InDialogue:
+		return	
+		
 	var translation = get_global_transform().origin
 	_global_datas.player_position = translation
 	_global_datas.player_boat_rotation = rotation
 	
-	if _global_datas.Player_In_Inventory:
-		return
 	
-	if _global_datas.Player_InSubScene:
-		return
-	if _global_datas.Player_InMenu:
-		return
-		
-	if _global_datas.Player_InDialogue:
-		return	
-			
+	if !using_pad:
+		move_a()
+	else:
+		move_b()
+
+func move_a():
+	
 	var forward_value = 0.0
 	if Input.is_action_pressed(("move_forward")):
 		var forward_vector = -transform.basis.z
@@ -50,76 +64,48 @@ func move_a():
 	
 	self.transform.origin.y = 0.0
 
-
 func move_b():
 	
-	
-	if _global_datas.Player_In_Inventory:
-		return
-	
-	if _global_datas.Player_InSubScene:
-		return
-	if _global_datas.Player_InMenu:
-		return
-		
-	if _global_datas.Player_InDialogue:
-		return	
-	
-	var direction = Vector3.ZERO;
-	var translation = get_global_transform().origin
-	_global_datas.player_position = translation
-	
-	if Input.is_action_pressed(("move_right")):
-		direction.x += 1
-	if Input.is_action_pressed(("move_left")):  
-		direction.x -= 1
-	if Input.is_action_pressed(("move_forward")):
-		direction.z -= 1	
-	if Input.is_action_pressed(("move_backward")):
-		direction.z += 1	
-		
-	# add movement	
-	if(translation + direction != translation):
-		look_at(translation + direction, Vector3.UP)
 
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()	
-		
-	apply_central_force(direction * move_speed)		
-	self.transform.origin.y = 0.0
-
-func move_c():
-	
-	
-	var translation = get_global_transform().origin
-	_global_datas.player_position = translation
-	_global_datas.player_boat_rotation = rotation
-	
 	var velocity = Input.get_vector("move_right", "move_left","move_backward" , "move_forward")
-	#if velocity != Vector2.ZERO:
-		#print(velocity.length())
-	
-	#var engine : int
-	#if Input.is_action_pressed("boat_engine_forward"):
-		#engine = 1
-		#var forward_vector = -transform.basis.z
-		#apply_central_force(forward_vector * 5 * engine)	
-		#
-	#if Input.is_action_pressed("boat_engine_backward"):
-		#engine = -1	
-		#var forward_vector = transform.basis.z
-		#apply_central_force(-forward_vector * 5 * engine)			
+
+	var engine : int
+	if Input.is_action_pressed("boat_engine_forward"):
+		engine = 1
+		var forward_vector = -transform.basis.z
+		apply_central_force(forward_vector * 5 * engine)	
 		
-	if velocity.length_squared() > 0.0:
-		# Calculate the angle of rotation based on input velocity
-		var target_rotation = atan2(velocity.x, velocity.y)
-		var velocity_magnitude = velocity.length()
-		# Smoothly rotate towards the target rotation
-		rotation.y = lerp_angle(rotation.y, target_rotation, 0.01)
-		self.transform.origin.y = 0.0
+	if Input.is_action_pressed("boat_engine_backward"):
+		engine = -1	
 		var forward_vector = transform.basis.z
-		apply_central_force(-forward_vector * 5 * velocity_magnitude)	
+		apply_central_force(-forward_vector * 5 * engine)			
 		
+	 # Calculate the torque based on input velocity
+	if velocity.length_squared() > 0.0:
+		var target_rotation = atan2(velocity.x, velocity.y)
+		var current_rotation = get_rotation().y
+
+		# Calculate the difference in rotation
+		var rotation_difference = wrap_angle(target_rotation - current_rotation)
+
+		 # Determine the direction of torque (1 or -1)
+		var torque_direction = 0.0
+		if rotation_difference > 0.0:
+			torque_direction = 1.0
+		elif rotation_difference < 0.0:
+			torque_direction = -1.0
+		else:
+			torque_direction = 0.0  # No torque needed if rotation_difference is zero
+
+
+		var current_velocity = linear_velocity
+		var current_speed = current_velocity.length()
+		# Apply torque to rotate towards the target rotation
+		var torque_vector = Vector3(0.0, torque_direction * current_speed * rotation_speed, 0.0)
+		apply_torque(torque_vector)
+
+	self.transform.origin.y = 0.0
+	
 # Helper function to smoothly interpolate between angles
 func lerp_angle(from, to, weight):
 	var difference = wrap_angle(to - from)
--- 100_GAME_MENU/Set_Resolution.gd
+++ 100_GAME_MENU/Set_Resolution.gd
@@ -25,7 +25,7 @@ func set_resolution(index):
 		get_window().content_scale_size = Vector2(1280,720)
 		_global_datas.set_resolution.emit(Vector2(1280,720))
 	
-		RenderingServer.global_shader_parameter_set("Dither_size",0.4)
+		RenderingServer.global_shader_parameter_set("Dither_size",0.3)
 		RenderingServer.global_shader_parameter_set("Dither_blue_noise_size",10)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		print("SCREEN SIZE",Vector2(1280,720))
@@ -40,10 +40,4 @@ func set_resolution(index):
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 		print("SCREEN SIZE",Vector2(1280,800))		
 		
-	if current_selected == 3:
-		_global_datas.set_resolution.emit(Vector2(1920,1080))
-		get_window().content_scale_size = Vector2(1920,1080)
-	
-		RenderingServer.global_shader_parameter_set("Dither_size",0.4)
-		RenderingServer.global_shader_parameter_set("Dither_blue_noise_size",10)	
-		print("SCREEN SIZE",Vector2(1920,1080))		
+
