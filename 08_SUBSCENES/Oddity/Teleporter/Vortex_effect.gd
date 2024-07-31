4f53240987dfa398eb57ad562be72924c7041525
Cirlu <leprovostulric@gmail.com>
2024-07-31 15:10:56 +0200

    yeahhh

--- 08_SUBSCENES/Oddity/Teleporter/Vortex_effect.gd
+++ 08_SUBSCENES/Oddity/Teleporter/Vortex_effect.gd
@@ -19,7 +19,7 @@ func scale_effect():
 	if t:
 		t.kill()
 	t = create_tween()
-	t.tween_method(_value,1.0,0.25,0.15).set_trans(Tween.TRANS_SINE)
+	t.tween_method(_value,1.0,0.01,0.15).set_trans(Tween.TRANS_SINE)
 	t.connect("finished",back_sacle)
 
 func back_sacle():
@@ -29,7 +29,7 @@ func back_sacle():
 	if t:
 		t.kill()
 		t = create_tween()
-		t.tween_method(_value,0.25,1.0,timer.wait_time).set_trans(Tween.TRANS_SINE)
+		t.tween_method(_value,0.01,1.0,timer.wait_time).set_trans(Tween.TRANS_SINE)
 		
 func _value(value : float):
 	teleporter_prefab.scale = Vector3(value,value,value)
