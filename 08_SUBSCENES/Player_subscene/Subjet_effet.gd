ac458fc3cbea79c4706970af9b87c924439d8921
Cirlu <leprovostulric@gmail.com>
2024-08-08 12:42:09 +0200

    test

--- 08_SUBSCENES/Player_subscene/Subjet_effet.gd
+++ 08_SUBSCENES/Player_subscene/Subjet_effet.gd
@@ -9,11 +9,18 @@ var _push_top : bool = false
 func _ready():
 	
 	subjet_effect.tool_active_signal.connect(_active)
+	_global_datas._backFrom_subscene.connect(reset)
 	
+	
+func reset():
+	player_sub.stop_control = false
+	_push_top = false
+		
 func _active(condition : bool):
 
-	player_sub.stop_control = condition
-	_push_top = condition
+	if condition:
+		player_sub.stop_control = condition
+		_push_top = condition
 	
 func _process(delta):
 	
