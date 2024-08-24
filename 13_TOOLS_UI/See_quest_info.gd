ea6011f45a89dcf690c2532459e73dd2f10430c0
Cirlu <leprovostulric@gmail.com>
2024-08-24 14:50:46 +0200

    wip

--- 13_TOOLS_UI/See_quest_info.gd
+++ 13_TOOLS_UI/See_quest_info.gd
@@ -3,7 +3,8 @@ extends Node
 @onready var quest_responce_panel = $".."
 
 func _ready():
-	_global_datas.open_quest_info.connect(open_question_info)
+	pass
+	#_global_datas.open_quest_info.connect(open_question_info)
 	
 	
 func open_question_info(condition : bool , quest : Note_Data):
