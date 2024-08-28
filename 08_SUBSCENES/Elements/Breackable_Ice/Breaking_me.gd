a4dc361c7e458e7be8e78148c4698d1a14900012
Cirlu <leprovostulric@gmail.com>
2024-08-28 23:19:29 +0200

    repairing_01

--- 08_SUBSCENES/Elements/Breackable_Ice/Breaking_me.gd
+++ 08_SUBSCENES/Elements/Breackable_Ice/Breaking_me.gd
@@ -5,13 +5,11 @@ extends Node
 const EXPLOSED_ROCK_VFX = preload("res://08_SUBSCENES/Elements/Breackable_Ice/Block_test/explosed_rock_vfx.tscn")
 var hit_index : int 
 
-@export var fractures : Array[RigidBody3D]
-
 func _hit():
 	
 	hit_index += 1
 	
-	if hit_index >= 1:
+	if hit_index >= 3:
 		explose_rock()
 		
 
@@ -28,9 +26,6 @@ func explose_rock():
 			var random_position = Vector3(randf_range(-1,1),randf_range(-1,1),randf_range(-1,1))
 		
 			child.apply_central_force(random_position * 10)
-			
-			
-	show_fractures()
 	root.queue_free()
 	
 
@@ -39,14 +34,3 @@ func instance_vfx():
 	var vfx = EXPLOSED_ROCK_VFX.instantiate()
 	_global_datas.LoadScene.add_child(vfx)
 	vfx.global_position = root.global_position
-
-func show_fractures():
-	var random_position = Vector3(randf_range(-1,1),randf_range(-1,1),randf_range(-1,1))
-	if fractures:
-		for f in fractures:
-			f.visible = true
-			f.freeze = false
-			var random_rotation = Vector3(randf_range(-90,90),randf_range(-1,1),randf_range(-180,180))
-			f.apply_central_force(random_position * 50)		
-			f.apply_torque(random_rotation * 0.1)		
-			f._fade_me()
