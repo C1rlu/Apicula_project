3db6eb800ec3b8e51d35c0126acba414c5657960
Cirlu <leprovostulric@gmail.com>
2024-09-27 19:17:20 +0200

    thats t he way to go

--- 00_SYSTEMS/Active_mask_at_ready.tres.gd
+++ 00_SYSTEMS/Active_mask_at_ready.tres.gd
@@ -3,9 +3,13 @@ extends Node
 
 @export var _active : Array[MeshInstance3D]
 
-
+@export var _active_node : Array[Node3D]
 func _ready():
 	
 	if _active:
 		for e in _active:
+			e.visible = true
+
+	if _active_node:
+		for e in _active_node:
 			e.visible = true
