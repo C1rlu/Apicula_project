7a930eb0673f7e80c5b7041fb9fbc97725b42917
Cirlu <leprovostulric@gmail.com>
2024-09-28 13:31:34 +0200

    update

--- 04_SCENE_MAIN_MAP/ELEMENTS/Boue/Boue_prefab.gd
+++ 04_SCENE_MAIN_MAP/ELEMENTS/Boue/Boue_prefab.gd
@@ -9,18 +9,14 @@ func _ready():
 	zone_datas.Boue_node_3D = self
 	zone_datas.fade_out = $Epave_visuals/Fade_out
 		
-	if zone_datas.Boue_active:
-		_active_boue()
-	else:
-		_disable_boue()	
+	_active_boue()	
 		
 func _disable_boue():
 	Dive_col.disabled = true
 	self.visible = false
 	
 func _active_boue():
-	
-	zone_datas.Boue_active = true
+
 	self.visible = true
 	Dive_col.visible = true
 	Dive_col.disabled = false
--- 15_GAME_RESSOURCE/00_Zone_data/zone_data.gd
+++ 15_GAME_RESSOURCE/00_Zone_data/zone_data.gd
@@ -3,10 +3,6 @@ class_name ZoneData
 
 @export var zone_node_dialogue : String = "set a zone node"
 @export var zone_packed_scene : PackedScene
-@export var Boue_active : bool = false
-
-@export var purchass_state : Array[purchass_data]
-var purchass_index_state : int = 0
 
 var Boue_node_3D : Node3D
 var fade_out : Node
